71eaf618b60b0f0a9e31de8154d81e3afd04ac4d
KaiqueBifulgo <kaiquebwork@hotmail.com>
2025-03-10 22:26:18 -0300

    javaScript iniciado

---
+++ js/script.js
@@ -0,0 +1,50 @@
+// ELEMENTOS
+const notesContainer = document.querySelector("#notes-container");
+
+const noteInput = document.querySelector("#note-content");
+
+const addNoteBtn = document.querySelector(".add-note");
+
+
+
+// FUNÇÕES
+function addNote() {
+
+    const noteObject = {
+        id: generateId,
+        content: noteInput.value,
+        fixed: false,
+    }
+
+    const noteElement = creatNote(noteObject.id, noteInput.content)
+
+    notesContainer.appendChild(noteElement);
+
+}
+
+function generateId () {
+    return Math.floor(Math.random() * 5000);
+}
+
+function creatNote(id, content, fixed) {
+
+    const element = document.createElement("div");
+
+    element.classList.add("note");
+
+    const textArea = document.createElement("textarea");
+
+    textArea.value = content;
+
+    textArea.placeholder = "Adicione uma nota...";
+
+    element.appendChild(textArea);
+
+    return element;
+
+}
+
+
+
+// EVENTOS
+addNoteBtn.addEventListener("click", () => addNote());
